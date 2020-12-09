ace074a8d94e416333e2e8b8eeafb773930af0de
Pinot. Kim <pinot.kim@kakao.com>
2020-12-09 17:03:29 +0900

    :lipstick: Add Mock UI

--- src/screen/HomeScreen/HomeScreen.tsx
+++ src/screen/HomeScreen/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import {SafeAreaView, View, Text, Image, Dimensions} from 'react-native';
 
 interface HomeScreenProps {
 }
@@ -13,7 +13,50 @@ const HomeScreen: React.FC<HomeScreenProps> = () => {
           alignItems: 'center',
           justifyContent: 'center',
         }}>
-        <Text>Home Screen</Text>
+        <View
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-start',
+            width: Dimensions.get('window').width,
+            padding: 15,
+          }}>
+          <Image
+            source={{
+              uri:
+                'https://bwipjs-api.metafloor.com/?bcid=code128&text=AB1234567890&scale=3',
+            }}
+            style={{
+              width: '100%',
+              height: 100,
+            }}
+            resizeMode={'stretch'}
+          />
+          <Text>2019-1234-5555</Text>
+          <Text>100,000 원</Text>
+        </View>
+
+        <View
+          style={{
+            padding: 15,
+          }}>
+          <Text>주문 스탬프</Text>
+          <Text>9 / 12 개 (75%)</Text>
+        </View>
+
+        <View
+          style={{
+            padding: 15,
+          }}>
+          <Text>주문하기</Text>
+        </View>
+
+        <View
+          style={{
+            padding: 15,
+          }}>
+          <Text>공지사항</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
