8626b69de9aaa0cc1d33bb1b0704ba70e0badd6d
Pinot. Kim <pinot.kim@kakao.com>
2020-12-09 15:13:10 +0900

    :wrench: Add Screen alias

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,26 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['babel-plugin-styled-components'],
+  plugins: [
+    'styled-components',
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@Screen': ['./src/screen'],
+        },
+      },
+    ],
+  ],
 };
