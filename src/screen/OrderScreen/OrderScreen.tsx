cb2e9d6cf95eab1aeefeb64ea8cdc6ff4f40a3fc
Pinot. Kim <pinot.kim@kakao.com>
2020-12-13 19:55:32 +0900

    :lipstick: Update OrderScreen Content

--- src/screen/OrderScreen/OrderScreen.tsx
+++ src/screen/OrderScreen/OrderScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, View, Image, StyleSheet} from 'react-native';
 import {
   List,
   Text,
@@ -7,9 +7,12 @@ import {
   TopNavigation,
   TopNavigationAction,
   Icon,
+  ListItem,
+  Layout,
 } from '@ui-kitten/components';
 import {ProductInfoType} from '@interface/types';
 import {StackNavigationProp} from '@react-navigation/stack';
+import {localizeCredit} from '@lib/utils';
 
 interface OrderScreenProps {
   navigation: StackNavigationProp<any>;
@@ -25,6 +28,10 @@ const OrderScreen: React.FC<OrderScreenProps> = ({navigation}) => {
       name: '아이스 티',
       price: 1000,
     },
+    {
+      name: '핫초코',
+      price: 1000,
+    },
   ];
   const BackAction = () => (
     <TopNavigationAction
@@ -32,30 +39,62 @@ const OrderScreen: React.FC<OrderScreenProps> = ({navigation}) => {
       icon={(props) => <Icon {...props} name="arrow-back" />}
     />
   );
-  const menus = ({item: {name, price}}: {item: ProductInfoType}) => (
-    <View>
-      <Text category="h5">{name}</Text>
-      <Text category="s2">{price} 원</Text>
-    </View>
-  );
   return (
     <SafeAreaView>
       <TopNavigation
         alignment="center"
-        title={(evaProps) => (
-          <Text {...evaProps}>
-            주문하기
-          </Text>
-        )}
+        title={(evaProps) => <Text {...evaProps}>주문하기</Text>}
         accessoryLeft={BackAction}
       />
       <Divider />
       <View>
-        <Text category="h2">주문 가능한 메뉴</Text>
-        <List data={data} renderItem={menus} ItemSeparatorComponent={Divider} />
+        <List
+          data={data}
+          style={styles.list}
+          renderItem={({item: {name, price}}: {item: ProductInfoType}) => (
+            <ListItem
+              accessoryLeft={() => (
+                <Image
+                  style={{
+                    width: 60,
+                    height: 60,
+                    borderRadius: 500,
+                  }}
+                  source={{
+                    uri: 'http://placehold.it/500x500',
+                  }}
+                />
+              )}
+              title={() => <Text style={styles.name}>{name}</Text>}
+              description={() => (
+                <Text style={styles.price}>{localizeCredit(price)}</Text>
+              )}
+            />
+          )}
+          ItemSeparatorComponent={Divider}
+        />
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  list: {
+    backgroundColor: 'transparent',
+    paddingHorizontal: 15,
+  },
+  card: {
+    marginTop: 20,
+  },
+  name: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginLeft: 7.5,
+  },
+  price: {
+    fontSize: 13,
+    marginLeft: 7.5,
+  },
+});
 
 export default OrderScreen;
